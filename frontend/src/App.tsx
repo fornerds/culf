86257f79c78f7f706b63705a69e98b95ad912d41
rieact <devrieact@gmail.com>
2024-12-08 22:00:36 +0900

    #26 feat: 이메일 찾기 UI 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -165,6 +165,11 @@ function AppRoutes() {
       setTitle('회원가입');
       setShowBackButton(true);
       setShowMenuButton(false);
+    } else if (matchPath('/find-email', pathname)) {
+      setUseHeader(true);
+      setTitle('이메일 찾기');
+      setShowBackButton(true);
+      setShowMenuButton(false);
     } else if (matchPath('/mypage/:tab', pathname)) {
       setUseHeader(true);
       setTitle('마이페이지');
@@ -260,7 +265,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter basename="/beta">
-         <AppRoutes />
+        <AppRoutes />
       </BrowserRouter>
       {import.meta.env.MODE === 'development' && (
         <ReactQueryDevtools initialIsOpen={false} />
--- frontend/src/pages/FindEmail/FindEmail.tsx
+++ frontend/src/pages/FindEmail/FindEmail.tsx
@@ -1,5 +1,88 @@
+import { InputBox } from '@/components/molecule/InputBox';
 import styles from './FindEmail.module.css';
+import { useState } from 'react';
+import { schemas } from '@/utils/validation';
+import { Button } from '@/components/atom';
 
 export function FindEmail() {
-  return <></>;
+  const [form, setForm] = useState({
+    phoneNumber: '',
+    birthDate: '',
+  });
+  const [formMessage, setFormMessage] = useState({
+    phoneNumber: '',
+    birthDate: '',
+  });
+
+  const validationCheck = async (id: string, value: string) => {
+    try {
+      const schema = schemas[id];
+      if (schema) {
+        await schema.validate(value);
+      }
+      return true;
+    } catch (validationError: any) {
+      setFormMessage((prev) => ({ ...prev, [id]: validationError.message }));
+      return false;
+    }
+  };
+
+  const handleFormChange = async (id: string, value: string) => {
+    setForm({
+      ...form,
+      [id]: value,
+    });
+    setFormMessage({
+      ...formMessage,
+      [id]: '',
+    });
+
+    validationCheck(id, value);
+  };
+
+  const isFormValid = () => {
+    return (
+      Object.values(form).every((value) => value) &&
+      Object.values(formMessage).every((message) => !message)
+    );
+  };
+
+  const hanelFindEmail = async () => {};
+
+  return (
+    <>
+      <main className={styles.main}>
+        <div className="font-title-1">이메일 찾기</div>
+        <div className={styles.inputGroup}>
+          <InputBox
+            id="phoneNumber"
+            label="휴대폰 번호"
+            placeholder="휴대폰 번호를 입력하세요"
+            value={form.phoneNumber}
+            validationMessage={formMessage.phoneNumber}
+            validationMessageType="error"
+            onChangeObj={handleFormChange}
+          />
+          <InputBox
+            id="birthDate"
+            label="생년월일"
+            placeholder="생년월일을 입력하세요"
+            value={form.birthDate}
+            validationMessage={formMessage.birthDate}
+            validationMessageType="error"
+            onChangeObj={handleFormChange}
+          />
+        </div>
+        <div className={styles.buttonArea}>
+          <Button
+            variant={!isFormValid() ? 'disable' : 'default'}
+            disabled={!isFormValid()}
+            onClick={hanelFindEmail}
+          >
+            이메일 찾기
+          </Button>
+        </div>
+      </main>
+    </>
+  );
 }
