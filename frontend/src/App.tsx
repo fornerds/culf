ab8ddafee2d3d2138cde4a252b95a5cb4c0ac468
KANGPUNGYUN <zkdvnd@naver.com>
2024-12-09 22:27:27 +0900

    feat: tanstack devtool 주석처리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ import {
   Outlet,
 } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useHeaderStore } from './state/client/useHeaderStore';
 import { useSideMenuStore } from './state/client/useSideMenuStore';
 
@@ -262,9 +262,9 @@ function App() {
       <BrowserRouter basename="/beta">
          <AppRoutes />
       </BrowserRouter>
-      {import.meta.env.MODE === 'development' && (
+      {/* {import.meta.env.MODE === 'development' && (
         <ReactQueryDevtools initialIsOpen={false} />
-      )}
+      )} */}
     </QueryClientProvider>
   );
 }
