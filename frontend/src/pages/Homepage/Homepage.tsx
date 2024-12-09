c311ab933402573a2326ef91d5777110f1c0b696
KANGPUNGYUN <zkdvnd@naver.com>
2024-12-09 14:26:11 +0900

    feat: 지난 대화 목록 없음 메시지 고정

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -1,21 +1,22 @@
+import { useState } from 'react';
 import styles from './Homepage.module.css';
 import curatorimage from '../../assets/images/curator01.png';
 import { Header, Cards, ChatList } from '../../components/organism';
 import { Footer } from '../../components/molecule';
 import { HeroBanner, SlideItem } from '../../modules';
 
-// 이미지 URL을 생성하는 헬퍼 함수
 function getImageUrl(name: string): string {
   return new URL(`../../assets/images/${name}`, import.meta.url).href;
 }
 
 export function Homepage() {
-  // 슬라이드 데이터
+  const chats: string | any[] = [];
+
   const slides: SlideItem[] = [
-    { imageUrl: getImageUrl('herobanner01.png'), link: '/mypage' },
-    { imageUrl: getImageUrl('herobanner02.png'), link: '/mypage' },
-    { imageUrl: getImageUrl('herobanner03.png'), link: '/mypage' },
-    { imageUrl: getImageUrl('herobanner04.png'), link: '/mypage' },
+    { imageUrl: getImageUrl('herobanner01.png'), link: 'http://pf.kakao.com/_KxoAdn' },
+    { imageUrl: getImageUrl('herobanner02.png'), link: '/' },
+    { imageUrl: getImageUrl('herobanner03.png'), link: '/' },
+    { imageUrl: getImageUrl('herobanner04.png'), link: '/' },
   ];
 
   const cardsData = [
@@ -51,46 +52,11 @@ export function Homepage() {
     },
   ];
 
-  const chatsData = [
-    {
-      id: '1',
-      curatorImage: { curatorimage },
-      curatorName: '국내여행 큐레이터 코리',
-      lastMessage:
-        '서울에서 꼭 가봐야할 맛집은 취향에 따라 달라질 수 있겠지만 저는 이 맛집을 추천합니다!',
-      lastMessageDate: '오전 12:47',
-    },
-    {
-      id: '2',
-      curatorImage: { curatorimage },
-      curatorName: '문화 큐레이터 컬리',
-      lastMessage: '이렇게 여행계획을 세우는 것은 어떨까요?',
-      lastMessageDate: '어제',
-    },
-    {
-      id: '3',
-      curatorImage: { curatorimage },
-      curatorName: '해외여행 큐레이터 몰리',
-      lastMessage:
-        '문의하신 콘서트는 이미 티켓이 마감되었어요 취향에 따라 달라질 수 있겠지만 저는 이 맛집을 추천합니다!',
-      lastMessageDate: '8월 2일',
-    },
-    {
-      id: '4',
-      curatorImage: { curatorimage },
-      curatorName: '미술관 큐레이터 아티',
-      lastMessage: '모나 미술관에서 진행중인 상설 전시는 두 가지가 있습니다',
-      lastMessageDate: '8월 2일',
-    },
-    {
-      id: '5',
-      curatorImage: { curatorimage },
-      curatorName: '국내여행 큐레이터 코리',
-      lastMessage:
-        '서울에서 꼭 가봐야할 맛집은 취향에 따라 달라질 수 있겠지만 저는 이 맛집을 추천합니다!',
-      lastMessageDate: '8월 1일',
-    },
-  ];
+  const NoChats = () => (
+    <div className={styles.noneChatList}>
+      지난 대화 목록이 없습니다.
+    </div>
+  );
 
   return (
     <>
@@ -108,7 +74,7 @@ export function Homepage() {
             <br />
             이어가 볼까요?
           </h2>
-          <ChatList chats={chatsData} />
+          {chats.length > 0 ? <ChatList chats={chats} /> : <NoChats />}
         </section>
       </main>
       <Footer />
